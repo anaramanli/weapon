fa2d2de26638c40a7c1159c587bc6bd28e626250
anaramanli <amanlianar@gmail.com>
2024-04-03 01:15:18 +0400

    adding new features

--- ConsoleApp1/ConsoleApp1/Models/Weapon.cs
+++ ConsoleApp1/ConsoleApp1/Models/Weapon.cs
@@ -8,10 +8,10 @@ namespace ConsoleApp1.Models
 {
     internal class Weapon
     {
-        private int BulletCapacity;
-        private int Bullets;
-        private int DischargeSecond;
-        private bool FireMode;
+        public int BulletCapacity;
+        public int Bullets;
+        public int DischargeSecond;
+        public bool FireMode;
 
 
         public Weapon(int bulletcapacity, int bullets, int dischargesec, bool mode)
@@ -21,26 +21,113 @@ namespace ConsoleApp1.Models
             DischargeSecond = dischargesec;
             FireMode = mode;
         }
-        /*public int Shoo
-        {
-            get => BulletCapacity;
-            set
-            {
-
-            }
-        }*/
         public void Shoot()
         {
-
-            if (Bullets > 0)
+            if (!FireMode)
             {
-                Bullets--;
-                Console.WriteLine("Pew Pew");
+                if (Bullets > 0)
+                {
+                    Bullets--;
+                    Console.WriteLine("Pew Pew" + "\nBullets Remain:" + $"{Bullets}");
+                }
+                else
+                {
+                    Console.WriteLine("Empty");
+                }
             }
             else
             {
-                Console.WriteLine("Empty");
+                Console.WriteLine("You have to change to single mode");
             }
+
+        }
+        public void Fire()
+        {
+            if (FireMode)
+            {
+                if (Bullets > 0)
+                {
+                    Console.WriteLine($"Pew pew pew.. ({Bullets} bullets fired)");
+                    Bullets = 0;
+                }
+                else
+                {
+                    Console.WriteLine("You dont have any bullets");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You have to change to Auto mode");
+            }
+
+
+        }
+        public int Capacity
+        {
+            get => BulletCapacity - Bullets;
+        }
+        public void Reload()
+        {
+            /*if (Bullets == BulletCapacity)
+            {
+                Console.WriteLine("Full bullets");
+                return;
+            }
+            else
+            {
+                Bullets = BulletCapacity;
+            }*/
+            if (Bullets < BulletCapacity)
+            {
+                Bullets = BulletCapacity;
+                Console.WriteLine("Reloading");
+            }
+            else
+            {
+                Console.WriteLine("Full bullets");
+            }
+        }
+
+        public bool Mode()
+        {
+            FireMode = !FireMode;
+            Console.WriteLine("Fire mode changed:" + FireMode);
+            return false;
+        }
+
+        public void ChangeCapacity(int newcapacity)
+        {
+            if (Bullets > newcapacity)
+            {
+                Bullets = 0;
+                Console.WriteLine("All bullets are removing");
+                Console.WriteLine($"New Capacity is{newcapacity}");
+            }
+            else
+            {
+                BulletCapacity = newcapacity;
+                Console.WriteLine($"New capacity number is {newcapacity}");
+            }
+        }
+
+        public void NewBullets(int newbullets)
+        {
+            if (newbullets <= BulletCapacity)
+            {
+                Bullets = newbullets;
+                Console.WriteLine($"New bullet is:{newbullets}");
+
+            }
+            else
+            {
+                Console.WriteLine("Bullets cant bigger than capacity");
+            }
+        }
+
+        public void NewTime(int newtime)
+        {
+            DischargeSecond = newtime;
+            Console.WriteLine($"New Discharge Second is {newtime}");
         }
     }
 
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,90 @@
-﻿namespace ConsoleApp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using ConsoleApp1.Models;
 
+namespace ConsoleApp1;
+internal class Program
+{
+    static void Main()
+    {
+        {
+            Start();
+        }
+    }
+    static void Start()
+    {
+        Weapon weapon = new Weapon(30, 0, 0, true);
+
+        while (true)
+        {
+        Restart:
+            Console.WriteLine("0 - İnformasiya almaq üçün\r\n1 - Shoot(Only in Single Mode) metodu üçün\r\n2 - Fire(Only in Auto Mode) metodu üçün\r\n3 - GetRemainBulletCount metodu üçün\r\n4 - Reload metodu üçün\r\n5 - ChangeFireMode metodu üçün\r\n6 - Proqramdan dayandırmaq üçün\r\nqısayoldur.\n7 - Redakte et :");
+            string input = Console.ReadLine();
+
+            if (input == "6")
+                break;
+            switch (input)
+            {
+                case "0":
+                    Console.WriteLine($"Bullets:{weapon.Bullets} \nFire Mode(True->Auto,False->Single):{weapon.FireMode}\nCapacity:{weapon.BulletCapacity}\nDischarge Second:{weapon.DischargeSecond}");
+                    break;
+                case "1":
+                    weapon.Shoot();
+                    break;
+                case "2":
+                    weapon.Fire();
+                    break;
+                case "3":
+                    Console.WriteLine($"You need to {weapon.Capacity} bullets to fill full");
+                    break;
+                case "4":
+                    weapon.Reload();
+                    break;
+                case "5":
+                    weapon.Mode();
+                    break;
+                case "7":
+                    NewWeapon(weapon);
+                    break;
+
+                default:
+                    Console.WriteLine("Choose Right Number");
+                    break;
+            }
+            Console.WriteLine("Press any key to clear . . .  ");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+
+    public static void NewWeapon(Weapon weapon)
+    {
+        while (true)
+        {
+            Console.WriteLine("T - Capacity Change\r\nS - Number of Bullets\r\nD - Discharge Second\nR-Return to Menu");
+            string input = Console.ReadLine().ToUpper();
+
+            Console.Clear();
+            switch (input)
+            {
+                case "T":
+                    Console.WriteLine("Enter new capacity");
+                    int newcapacity = Convert.ToInt32(Console.ReadLine());
+                    weapon.ChangeCapacity(newcapacity);
+                    break;
+                case "S":
+                    Console.WriteLine("Enter new bullets number");
+                    int newbullet = Convert.ToInt32(Console.ReadLine());
+                    weapon.NewBullets(newbullet);
+                    break;
+                case "D":
+                    Console.WriteLine("Enter new time");
+                    int newtime = Convert.ToInt32(Console.ReadLine());
+                    weapon.NewTime(newtime);
+                    break;
+                case "R":
+                    /*Start();*/
+                    /*goto Restart;*/
+                    return;
+            }
         }
     }
 }
