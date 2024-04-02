f571093ab1f861ca74fbb337c1e036d2d3f9bcb3
anaramanli <riadhamidov@gmail.com>
2024-04-02 13:50:56 +0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Models/Weapon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    internal class Weapon
+    {
+        private int BulletCapacity;
+        private int Bullets;
+        private int DischargeSecond;
+        private bool FireMode;
+
+
+        public Weapon(int bulletcapacity, int bullets, int dischargesec, bool mode)
+        {
+            BulletCapacity = bulletcapacity;
+            Bullets = bullets;
+            DischargeSecond = dischargesec;
+            FireMode = mode;
+        }
+        /*public int Shoo
+        {
+            get => BulletCapacity;
+            set
+            {
+
+            }
+        }*/
+        public void Shoot()
+        {
+
+            if (Bullets > 0)
+            {
+                Bullets--;
+                Console.WriteLine("Pew Pew");
+            }
+            else
+            {
+                Console.WriteLine("Empty");
+            }
+        }
+    }
+
+}
